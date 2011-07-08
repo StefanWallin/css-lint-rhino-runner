e8c9ba8f49409b8c57733e23527f1371bb9db039
Stefan Wallin <stefan.w@festiz.com>
2011-07-09 00:38:05 +0200

    Fixed issue #3: Added support for strict mode using -s parameter.

--- lint.sh
+++ lint.sh
@@ -4,12 +4,16 @@
 #	It then saves the warnings to the log file and echoes the errors and 
 #	returns a value equal to the number of errors.
 
+#Set up some initial vars:
+STRICTMODE=false
+
+
+
 # Init the temp file
 touch .tmp.txt
 
-case "$#" in
-0) while read line; do
-	echo "STDIN";
+#Loop through the arguments in STDIN
+while read line; do
 	line=(${line}); 
 	for (( i=0 ; i < ${#line[@]} ; i++ ))
 	do 
@@ -17,19 +21,20 @@ case "$#" in
 		find ${line[$i]} |grep -v '/css$' |grep '.css$' |grep -v 'min.css$' >> .tmp.txt
 	done
 done
-   ;;
--*|--*) echo help; exit 1
-   ;;
-*) args=($@);
-	echo "ARGS";
-	for (( i=1 ; i < $#; i++ ))
-	do 
 
-		# Find all files that end in .css but are not minified(which ends in .min.css).
-		find ${!i} |grep -v '/css$' |grep '.css$' |grep -v 'min.css$' >> .tmp.txt
-	done
-   ;;
-esac
+#Loop through arguments in command line 
+for (( i=1 ; i <= $#; i++ ))
+do 
+	case ${!i} in
+		"-s")
+			STRICTMODE=true
+			continue;
+			;;
+	esac
+	# Find all files that end in .css but are not minified(which ends in .min.css).
+	find ${!i} |grep -v '/css$' |grep '.css$' |grep -v 'min.css$' >> .tmp.txt
+done
+
 
 # Print which files is going to be linted
 echo "CSS linting these files:"
@@ -44,19 +49,38 @@ touch ./logs/csswarnings.log
 # Execute the linter and save the output in our log-file.
 java -jar rhino.jar csslint.js $MYFILES &> ./logs/csswarnings.log
 
-# Find out how many errors we had
-E=`cat logs/csswarnings.log |grep " error at"|wc -l`
+if [ $STRICTMODE == "true" ]; then
+	# Find out how many errors and warnings we had
+	E=`cat logs/csswarnings.log |grep " error at"|wc -l` || 0
+	W=`cat logs/csswarnings.log |grep " warning at"|wc -l` || 0
+	
+	# Print the errors to the console
+	cat logs/csswarnings.log
 
-# Print the errors to the console
-cat logs/csswarnings.log |grep -B 1 -A 3 " error at"
+	# Print how many errors we found:
+	echo "CSS linting found $E errors and $W warnings."
 
-# Print how many errors we found:
-echo "CSS linting found $E errors."
+	#Counting up errors to the result
+	R=$(($E+$W))
+	
+else
+	# Find out how many errors we had
+	E=`cat logs/csswarnings.log |grep " error at"|wc -l`
+
+	# Print the errors to the console
+	cat logs/csswarnings.log |grep -B 1 -A 3 " error at"
+
+	# Print how many errors we found:
+	echo "CSS linting found $E errors."
+
+	#Counting up errors to the result
+	R=$E
+fi
 
 # Remove our .tmp.txt-file.
 rm .tmp.txt
 
 #exit with the number of errors.
 # exit status greater than zero should halt the following executions.
-exit $E;
+exit $R;
 
