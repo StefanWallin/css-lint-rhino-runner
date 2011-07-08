8a9fa5890c1ec6ccc65b73c5396a33df8fb51857
Stefan Wallin <stefan.w@festiz.com>
2011-07-08 23:51:04 +0200

    Fix Issue #1: Add support for feeding folders as command line arguments.

--- lint.sh
+++ lint.sh
@@ -4,13 +4,16 @@
 #	It then saves the warnings to the log file and echoes the errors and 
 #	returns a value equal to the number of errors.
 
+# Init the temp file
+touch .tmp.txt
 
+# Loop through the argument folders: 
+for (( i=1 ; i < $#; i++ ))
+do 
+	# Find all files that end in .css but are not minified(which ends in .min.css).
+	find ${!i} |grep -v '/css$' |grep '.css$' |grep -v 'min.css$' >> .tmp.txt
+done
 
-# Which folders should we look in: 
-CSSDIR=/Users/stefan/Desktop/css
-
-# Find all files that end in .css but are not minified(which ends in .min.css).
-find $CSSDIR |grep -v '/css$' |grep '.css$' |grep -v 'min.css$' > .tmp.txt
 
 # Print which files is going to be linted
 echo "CSS linting these files:"
@@ -19,8 +22,11 @@ cat .tmp.txt
 # Find all newlines that separates the filenames and replace them with spaces
 MYFILES=`cat .tmp.txt |tr '\n' ' '`
 
+# Init the log file to avoid errors on nonexistant file
+touch ./logs/csswarnings.log
+
 # Execute the linter and save the output in our log-file.
-java -jar rhino.jar csslint.js $MYFILES > logs/csswarnings.log
+java -jar rhino.jar csslint.js $MYFILES &> ./logs/csswarnings.log
 
 # Find out how many errors we had
 E=`cat logs/csswarnings.log |grep " error at"|wc -l`
